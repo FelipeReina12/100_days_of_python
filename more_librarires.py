a4a3c2c8e69e57e10bdc60786b31b3532731897a
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2025-01-17 12:14:33 -0500

    Subiendo archivo

---
+++ more_librarires.py
@@ -0,0 +1,24 @@
+import os  #Importamos libreria para hablar con la terminal
+
+# for i in range(1, 1000):  #Creamos un ciclo for para generar numeros del 1 al 999
+#     print(i)  #Imprimimos esos numeros 
+#     os.system("clear")  #Con el comando os.system("clear") borrmos los numeros en la terminal
+
+
+# import time
+
+# print("""Hello Welcome to 
+# 100 Days of code 
+# Whit Python!!!!!!
+# """)
+# time.sleep(3)
+# os.system("clear")
+# user_name = input("What is your name? ")
+# print(f"Welcome, {user_name.capitalize()}")
+# time.sleep(3)
+# os.system("clear")
+# age = int(input("Enter your age: "))
+# print(f"You are {age} years old")
+# time.sleep(3)
+# os.system("clear")
+
